e7e601727790348e742a3ceaafb11dc0cbd210ad
Noah O <manwhoami@users.noreply.github.com>
2016-10-27 20:55:02 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,25 @@
 from socket import *
-import time
+import time, struct
+
+def recv_msg(sock):
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    return recvall(sock, msglen)
+
+def recvall(sock, n):
+    data = ''
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data += packet
+    return data
+
+def send_msg(sock, msg):
+    finalMsg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(finalMsg)
 
 #establish connection TO server
 serverName = '127.0.0.1'
